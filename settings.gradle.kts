f882c8d7302b0c31babf18c6d5ade579fa9c5862
Andriy Redko <andriy.redko@aiven.io>
2022-12-15 08:20:42 -0500

    Dependency updates (Spring Framework, mockito)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,8 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "6.0.2")
-      version("spring-boot", "3.0.0")
+      version("spring", "6.0.3")
+      version("spring-boot", "3.0.1")
       library("data-commons", "org.springframework.data:spring-data-commons:3.0.0")
       library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.0.0")
       library("web", "org.springframework", "spring-web").versionRef("spring")
