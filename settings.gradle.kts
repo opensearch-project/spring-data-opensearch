023d10beb90fd86a5d73a77ebd0dc3a490195308
Andriy Redko <andriy.redko@aiven.io>
2023-07-14 15:31:43 -0400

    Dependency updates: Spring 6.0.11, Spring Data Elasticsearch 5.0.8, Spring Data Commons 3.0.8
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,10 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "6.0.10")
+      version("spring", "6.0.11")
       version("spring-boot", "3.0.8")
-      library("data-commons", "org.springframework.data:spring-data-commons:3.0.7")
-      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.0.7")
+      library("data-commons", "org.springframework.data:spring-data-commons:3.0.8")
+      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.0.8")
       library("web", "org.springframework", "spring-web").versionRef("spring")
       library("context", "org.springframework", "spring-context").versionRef("spring")
       library("tx", "org.springframework", "spring-tx").versionRef("spring")
