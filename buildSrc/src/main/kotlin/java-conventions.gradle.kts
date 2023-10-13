715989fc9f79ece113ccb160a7939922a75a1c59
Andriy Redko <andriy.redko@aiven.io>
2023-10-13 12:36:11 -0400

    Dependency updates: Jackson 2.15.3, Mockito 5.6.0, Testcontainers 1.19.1, Spring Data Elasticsearch 5.1.5, Spring 6.0.13
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- buildSrc/src/main/kotlin/java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-conventions.gradle.kts
@@ -51,9 +51,9 @@ val catalogs = extensions.getByType<VersionCatalogsExtension>()
 val jupiter = catalogs.named("libs").findVersion("jupiter").get().requiredVersion
 
 dependencies {
-  testImplementation("org.testcontainers:testcontainers:1.18.0")
-  testImplementation("org.testcontainers:junit-jupiter:1.18.0")
-  testImplementation("org.mockito:mockito-junit-jupiter:5.3.1")
+  testImplementation("org.testcontainers:testcontainers:1.19.1")
+  testImplementation("org.testcontainers:junit-jupiter:1.19.1")
+  testImplementation("org.mockito:mockito-junit-jupiter:5.6.0")
   testImplementation("org.junit.jupiter:junit-jupiter:${jupiter}")
   testImplementation("org.junit.jupiter:junit-jupiter-params:${jupiter}")
   testImplementation("org.assertj:assertj-core:3.24.2")
