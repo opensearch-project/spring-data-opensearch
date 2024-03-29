3e7bd653b6c99fc8199c4af14ecdcf621ff47fba
Dongin.min <dongin.min@navercorp.com>
2024-03-29 15:59:14 +0900

    remove unused import
    Signed-off-by: Dongin.min <dongin.min@navercorp.com>

--- spring-data-opensearch-examples/spring-boot-gradle/src/main/java/org/opensearch/data/example/rest/MarketplaceRestController.java
+++ spring-data-opensearch-examples/spring-boot-gradle/src/main/java/org/opensearch/data/example/rest/MarketplaceRestController.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
