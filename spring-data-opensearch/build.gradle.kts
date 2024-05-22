1a8b575f3ce486889b30f9a7ea3046a9ff9dc8c7
Andriy Redko <andriy.redko@aiven.io>
2024-05-22 12:35:45 -0400

    Bump wiremock to 3.6.0 (#281)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- spring-data-opensearch/build.gradle.kts
+++ spring-data-opensearch/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
   testImplementation(springLibs.test) {
     exclude("ch.qos.logback", "logback-classic")
   }
-  testImplementation("org.wiremock:wiremock:3.5.4") {
+  testImplementation("org.wiremock:wiremock:3.6.0") {
     exclude("commons-logging", "commons-logging")
     exclude("org.ow2.asm", "asm")
   }
