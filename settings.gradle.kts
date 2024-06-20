c90869d907bfddc003f7f992ae4d3f76ade6acde
Andriy Redko <andriy.redko@aiven.io>
2024-06-20 18:14:09 -0400

    Dependency updates: Spring Boot 3.2.7, Spring Framework 6.1.10, Spring Data Elasticsearch 5.2.7, Spring Data Commons 3.2.7 (#292)
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,10 +14,10 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "6.1.8")
-      version("spring-boot", "3.2.6")
-      library("data-commons", "org.springframework.data:spring-data-commons:3.2.6")
-      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.2.6")
+      version("spring", "6.1.10")
+      version("spring-boot", "3.2.7")
+      library("data-commons", "org.springframework.data:spring-data-commons:3.2.7")
+      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.2.7")
       library("web", "org.springframework", "spring-web").versionRef("spring")
       library("context", "org.springframework", "spring-context").versionRef("spring")
       library("tx", "org.springframework", "spring-tx").versionRef("spring")
