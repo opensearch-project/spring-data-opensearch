822c32af2a44a3179166dfe8e43d4e57421ef3ae
Andriy Redko <drreta@gmail.com>
2025-08-25 21:54:37 -0400

    Update Spring to 6.2.10 and Spring Boot to 3.5.5
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,8 +16,8 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "6.2.8")
-      version("spring-boot", "3.5.3")
+      version("spring", "6.2.10")
+      version("spring-boot", "3.5.5")
       library("data-commons", "org.springframework.data:spring-data-commons:3.5.3")
       library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.5.3")
       library("web", "org.springframework", "spring-web").versionRef("spring")
