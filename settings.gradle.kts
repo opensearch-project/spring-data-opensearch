45faf10d51644f1661a470d4cbe2f486b3b2d8c8
Andriy Redko <drreta@gmail.com>
2025-12-11 18:21:26 -0500

    Update Spring Framework to 6.2.15
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,10 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "6.2.14")
+      version("spring", "6.2.15")
       version("spring-boot", "3.5.8")
-      library("data-commons", "org.springframework.data:spring-data-commons:3.5.6")
-      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.5.6")
+      library("data-commons", "org.springframework.data:spring-data-commons:3.5.7")
+      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:5.5.7")
       library("web", "org.springframework", "spring-web").versionRef("spring")
       library("webflux", "org.springframework", "spring-webflux").versionRef("spring")
       library("context", "org.springframework", "spring-context").versionRef("spring")
