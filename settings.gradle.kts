d6e08242f8d7372bd4f76ec5e7bbfd1b83d54e5d
Andriy Redko <drreta@gmail.com>
2025-12-11 18:18:16 -0500

    Update Spring Framework to 7.0.2
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,10 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "7.0.1")
+      version("spring", "7.0.2")
       version("spring-boot", "4.0.0")
-      library("data-commons", "org.springframework.data:spring-data-commons:4.0.0")
-      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:6.0.0")
+      library("data-commons", "org.springframework.data:spring-data-commons:4.0.1")
+      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:6.0.1")
       library("web", "org.springframework", "spring-web").versionRef("spring")
       library("webflux", "org.springframework", "spring-webflux").versionRef("spring")
       library("context", "org.springframework", "spring-context").versionRef("spring")
