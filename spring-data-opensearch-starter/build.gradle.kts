63e5da67d07175adb152ee29340e839587d821d9
Tomas Longo <tlongo@sternad.de>
2026-05-26 15:36:24 +0200

    Cleanup

--- spring-data-opensearch-starter/build.gradle.kts
+++ spring-data-opensearch-starter/build.gradle.kts
@@ -22,8 +22,7 @@ dependencies {
   api(project(":spring-data-opensearch"))
   api(springLibs.boot.data.elasticsearch)
   api(springLibs.boot.elasticsearch)
-  // todo tlongo
-  implementation("org.springframework.boot:spring-boot-starter-actuator:4.0.6")
+  implementation(springLibs.boot.starter.actuator)
   implementation(opensearchLibs.client) {
     exclude("commons-logging", "commons-logging")
     exclude("org.slf4j", "slf4j-api")
@@ -38,7 +37,6 @@ dependencies {
     exclude("ch.qos.logback", "logback-classic")
   }
   testImplementation(springLibs.boot.test)
-//  testImplementation(springLibs.boot.starter.actuator)
   testImplementation(opensearchLibs.testcontainers)
   testImplementation(jacksonLibs.core)
   testImplementation(jacksonLibs.databind)
