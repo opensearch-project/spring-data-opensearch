4b5966acfbe76af6c93aff7b378c03d2ad561154
Andriy Redko <drreta@gmail.com>
2026-08-20 18:31:23 -0400

    Spring Boot 4.2 baseline
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,10 +15,10 @@ dependencyResolutionManagement {
     }
 
     create("springLibs") {
-      version("spring", "7.0.9")
-      version("spring-boot", "4.1.1")
-      library("data-commons", "org.springframework.data:spring-data-commons:4.1.1")
-      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:6.1.1")
+      version("spring", "7.1.0-M2")
+      version("spring-boot", "4.2.0-M2")
+      library("data-commons", "org.springframework.data:spring-data-commons:4.2.0-M2")
+      library("data-elasticsearch", "org.springframework.data:spring-data-elasticsearch:6.2.0-M2")
       library("web", "org.springframework", "spring-web").versionRef("spring")
       library("webflux", "org.springframework", "spring-webflux").versionRef("spring")
       library("context", "org.springframework", "spring-context").versionRef("spring")
