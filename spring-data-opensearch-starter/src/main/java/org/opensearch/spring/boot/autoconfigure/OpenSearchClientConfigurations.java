c50835d7f343ef93fcf20501457396cf6b032fd6
Oualid <bouh.oualid@gmail.com>
2026-09-02 23:26:15 +0200

    Fix mapping customizer auto-configuration backoff (#826)
    * Fix mapping customizer auto-configuration backoff
    
    Signed-off-by: oualid.bouh <oualid.bouh@kleegroup.com>
    
    * Update spring-data-opensearch-starter/src/main/java/org/opensearch/spring/boot/autoconfigure/OpenSearchClientConfigurations.java
    
    Co-authored-by: Andriy Redko <drreta@gmail.com>
    Signed-off-by: Oualid <bouh.oualid@gmail.com>
    
    * Apply suggestion from @reta
    
    Signed-off-by: Andriy Redko <drreta@gmail.com>
    
    ---------
    
    Signed-off-by: oualid.bouh <oualid.bouh@kleegroup.com>
    Signed-off-by: Oualid <bouh.oualid@gmail.com>
    Signed-off-by: Andriy Redko <drreta@gmail.com>
    Co-authored-by: oualid.bouh <oualid.bouh@kleegroup.com>
    Co-authored-by: Andriy Redko <drreta@gmail.com>

--- spring-data-opensearch-starter/src/main/java/org/opensearch/spring/boot/autoconfigure/OpenSearchClientConfigurations.java
+++ spring-data-opensearch-starter/src/main/java/org/opensearch/spring/boot/autoconfigure/OpenSearchClientConfigurations.java
@@ -23,6 +23,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.elasticsearch.core.index.MappingParametersCustomizer;
 import tools.jackson.databind.ObjectMapper;
 
 class OpenSearchClientConfigurations {
@@ -74,7 +75,7 @@ class OpenSearchClientConfigurations {
     static class OpenSearchMappingParametersCustomizerConfiguration {
         @Bean
         @ConditionalOnMissingBean
-        OpenSearchMappingParametersCustomizer opensearchMappingParametersCustomizer() {
+        MappingParametersCustomizer opensearchMappingParametersCustomizer() {
             return new OpenSearchMappingParametersCustomizer();
         }
     }
