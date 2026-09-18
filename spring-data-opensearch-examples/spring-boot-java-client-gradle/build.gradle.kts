f13bca2b14a81f6a750b4840348ce6f208d4edff
opensearch-ci <83309141+opensearch-ci-bot@users.noreply.github.com>
2026-09-18 07:54:43 -0700

    Bump org.graalvm.buildtools.native from 1.1.10 to 1.1.11 (#827) (#852)
    Bumps [org.graalvm.buildtools.native](https://github.com/graalvm/native-build-tools) from 1.1.10 to 1.1.11.
    - [Release notes](https://github.com/graalvm/native-build-tools/releases)
    - [Commits](https://github.com/graalvm/native-build-tools/compare/1.1.10...1.1.11)
    
    ---
    updated-dependencies:
    - dependency-name: org.graalvm.buildtools.native
      dependency-version: 1.1.11
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    
    
    (cherry picked from commit d0dde686c75ae7f9d6816c52958d7cff7f7d961a)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: opensearch-ci-bot <opensearch-infra@amazon.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- spring-data-opensearch-examples/spring-boot-java-client-gradle/build.gradle.kts
+++ spring-data-opensearch-examples/spring-boot-java-client-gradle/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
   alias(pluginLibs.plugins.spotless)
   alias(pluginLibs.plugins.editorconfig)
   id("java-conventions")
-  id("org.graalvm.buildtools.native") version "1.1.10"
+  id("org.graalvm.buildtools.native") version "1.1.11"
 }
 
 buildscript {
